4d202d08f2422e03fdef6960e6538033c3b2f1fe
Tadaka Rohith Kumar <113246394+rohithtadaka@users.noreply.github.com>
2024-10-03 14:28:25 +0530

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,21 @@
+function clearDisplay() {
+    document.getElementById('display').value = '';
+}
+
+function deleteLast() {
+    const display = document.getElementById('display');
+    display.value = display.value.slice(0, -1);
+}
+
+function appendToDisplay(value) {
+    document.getElementById('display').value += value;
+}
+
+function calculate() {
+    const display = document.getElementById('display');
+    try {
+        display.value = eval(display.value); // Perform the calculation
+    } catch (error) {
+        display.value = 'Error'; // Handle invalid expressions
+    }
+}
